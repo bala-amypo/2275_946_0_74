9a193f785f1e7aaf55080c9890bb0628bb6f0dad
bala-amypo <bala@amypo.in>
2025-12-17 08:35:14 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,41 +1,41 @@
-// package com.example.demo.service;
-
-// import java.util.List;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import com.example.demo.entity.Student;
-// import com.example.demo.repository.StudentRepository;
-
-// // @Service
-// public interface StudentService {
-
-//     @Autowired
-//     StudentRepository repo;
-
-//     //create (or) Insert data
-//      Student createData(Student stu);
-
-//     //Fetch all records
-//     List<Student>fetchRecord();
-
-// }
-
-
-
-
 package com.example.demo.service;
-// import java.util.HashMap;
+
 import java.util.List;
-// import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
-import repository.StudentRepository;
-@Service
+import com.example.demo.repository.StudentRepository;
+
+// @Service
 public interface StudentService {
 
-  Student createData(Student stu);
-  List<Student>fetch();
+    @Autowired
+    StudentRepository repo;
 
+    //create (or) Insert data
+     Student createData(Student stu);
+
+    //Fetch all records
+    List<Student>fetchRecord();
 
 }
+
+
+
+
+// package com.example.demo.service;
+// // import java.util.HashMap;
+// import java.util.List;
+// // import java.util.Map;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import com.example.demo.entity.Student;
+// import repository.StudentRepository;
+// @Service
+// public interface StudentService {
+
+//   Student createData(Student stu);
+//   List<Student>fetch();
+
+
+// }
