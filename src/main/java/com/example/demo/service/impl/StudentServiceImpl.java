615ddf5272e0642bd6fad8542c96bd280b0976a0
bala-amypo <bala@amypo.in>
2025-12-17 09:50:14 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -33,26 +33,67 @@
 package com.example.demo.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 
 @Service 
 public class StudentServiceImpl implements StudentService {
 
-    @Autowired
-    private StudentRepository repo;
 
-    @Override
-    public Student createData(Student stu) {
+    @Autowired
+    StudentRepository repo;
+
+    //create (or) Insert data
+    public Student createData(Student stu){
         return repo.save(stu);
     }
 
-    @Override
-    public List<Student> fetchRecord() {
+    //Fetch all records
+    public List<Student>fetchRecord(){
         return repo.findAll();
     }
+
+    public Optional<Student>fetchDataById(int id){
+        return repo.findById(id);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+// package com.example.demo.service.implementation;
+
+// import java.util.List;
+// import java.util.Optional;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import com.example.demo.entity.Student;
+// import com.example.demo.repository.StudentRepository;
+
+// public class StudentServiceImplementation {
+
+//     @Autowired
+//     StudentRepository repo;
+
+//     //create (or) Insert data
+//     public Student createData(Student stu){
+//         return repo.save(stu);
+//     }
+
+//     //Fetch all records
+//     public List<Student>fetchRecord(){
+//         return repo.findAll();
+//     }
+
+//     public Optional<Student>fetchDataById(int id){
+//         return repo.findById(id);
+//     }
+
+// }
